2d2eda51da0704c926d35794f7e497ca490452d7
xeezow <ignxeezow@gmail.com>
2021-09-05 17:29:53 +0200

    Working on adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { MatToolbarModule} from "@angular/material/toolbar";
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './component/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ContentComponent } from './content/content.component';
-import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './page/home/content/content.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './page/home/home.component';
+import { ProjectsComponent } from './page/projects/projects.component';
+
+const routes: Routes = [
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ContentComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
